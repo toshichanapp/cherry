9ab08a612d28cfa11352bcd4d4113fc8ae34a42d
Toshiki Fujiyoshi <toshichan@synapz.jp>
2018-05-02 22:43:13 +0900

    testと本体を分ける

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,15 +1,3 @@
-def fizz_buzz(n)
-  if n % 15 == 0
-    'FizzBuzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 == 0
-    'Buzz'
-  else
-    n.to_s
-  end
-end
-
 require 'minitest/autorun'
 
 class FizzBuzzTest < Minitest::Test

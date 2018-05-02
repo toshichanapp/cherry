ea80b9b44c2fab76cf806a116323a623f0bad44d
Toshiki Fujiyoshi <toshichan@synapz.jp>
2018-05-02 22:35:40 +0900

    fizzbuzz test追加

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -9,3 +9,14 @@ def fizz_buzz(n)
     n.to_s
   end
 end
+
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal 'FizzBuzz', fizz_buzz(16)
+  end
+end

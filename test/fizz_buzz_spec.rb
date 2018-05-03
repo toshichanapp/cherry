b04adf623d2ba2920ba277e6681d415e9a2d405f
Toshiki Fujiyoshi <toshichan@synapz.jp>
2018-05-03 11:03:23 +0900

    rspecによるテスト

---
+++ test/fizz_buzz_spec.rb
@@ -0,0 +1,9 @@
+require './lib/fizz_buzz'
+
+RSpec.describe 'Fizz Buzz' do
+  example 'fizz_buzz' do
+    expect(fizz_buzz(1)).to eq '1'
+    expect(fizz_buzz(2)).to eq '2'
+    expect(fizz_buzz(3)).to eq 'Fizz'
+  end
+end
